10b27a43a62b9a1c4e35a802e5d1a219953a9588
Raul Jimenez <officialrauljimenez@gmail.com>
2021-12-30 15:42:05 -0600

    feat(scrapper): scrapper now working on a per channel basis

--- src/DiscordScraper.js
+++ src/DiscordScraper.js
@@ -1,16 +1,76 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-require('dotenv').config();
+const { Client, Intents } = require("discord.js");
+const { DatabaseService } = require("./service/DatabaseService");
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+});
+require("dotenv").config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
+const DB_URL = process.env.DB_URL;
 
-client.login(process.env.JOHN_SHOUT_TOKEN);
+client.login(BOT_TOKEN);
+const databaseService = new DatabaseService(DB_URL);
 
+const channelsScrapped = [];
 
-client.on('ready', async () => {
-    // scrape audit logs
-    client.
-    // scrape messages
-
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.username}`);
 });
 
+client.on("messageCreate", async (message) => {
+  if (
+    message.author.username === "raulito" &&
+    message.content === "!unwrapped scrape channel"
+  ) {
+    await databaseService.createConnection();
+    // fetch messages
+    if (!channelsScrapped.includes(message.channelId)) {
+      console.log(`channelID: ${message.channelId} not scraped. Scrapping...`);
+      channelsScrapped.push(message.channelId);
+
+      const messageManager = message.channel.messages;
+      let nextSnowflake = message.id;
+      let channelProcessed = false;
+      let channelMessagesSize = BigInt(0);
+
+      while (!channelProcessed) {
+        console.log(`Processing Messages ending at snowflake ${nextSnowflake}`);
+        const messages = await messageManager.fetch({
+          limit: 100,
+          before: nextSnowflake,
+        });
+
+        channelMessagesSize += BigInt(messages.size);
+
+        console.log(
+          `Successful fetching of ${messages.size} messages with ending snowflake at ${nextSnowflake}, current count at ${channelMessagesSize}`
+        );
+
+        if (messages.size === 0) {
+          channelProcessed = true;
+          console.log(
+            `finished processing messages for channelID: ${message.channelId}`
+          );
+          message.reply(`finished storing ${channelMessagesSize}`);
+        } else {
+          for (let [id, message] of messages) {
+            console.log(JSON.stringify(message));
+            console.log(`Snowflake=${nextSnowflake} messageID=${id}`);
+            nextSnowflake =
+              BigInt(nextSnowflake) > BigInt(id) ? id : nextSnowflake;
+            await databaseService.saveMessage(message);
+          }
+        }
+      }
+      await databaseService.closeConnection();
+    } else {
+      console.log(`channelID: ${message.channelId} already scraped. `);
+    }
+
+    // save the messages into a mongo database
+  }
+});
--- src/service/DatabaseService.js
+++ src/service/DatabaseService.js
@@ -1,28 +1,30 @@
-export default class DatabaseService {
+class DatabaseService {
   constructor(dbResourceUrl) {
     this.dbResourceUrl = dbResourceUrl;
     this.connected = false;
   }
 
-
   async createConnection() {
     if (!this.connected) {
-      
       this.connected = true;
+      console.log(`established connection to ${this.dbResourceUrl}`);
+    } else {
+      console.log(`already connected to ${this.dbResourceUrl}`);
     }
-
   }
 
+  async saveMessage(message) {}
 
-  async saveMessage(message) {
-
-  }
+  async saveAuditLog(auditLog) {}
 
   async closeConnection() {
     if (!this.connected) {
-      
       this.connected = false;
+      console.log(`closed connection to ${this.dbResourceUrl}`);
+    } else {
+      console.log(`connection is already closed to ${this.dbResourceUrl}`);
     }
-    
   }
 }
+
+module.exports = { DatabaseService };
